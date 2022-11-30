d6f71b0bd3f2c1bc829cd8c4de1b201fb682ff33
Manas Jayaswal <mjmanas54@gmail.com>
2022-11-30 17:35:15 +0530

    updated

--- A_Exciting_Bets.cpp
+++ A_Exciting_Bets.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define rep(i,a,b) for(int i=a;i<b;i++)
 #define read(arr,n) for(int i=0;i<n;i++) cin>>arr[i];
-#define swap(a,b) int temp = a; a = b; b = temp;
+#define swap(a,b) unsigned long long int temp = a; a = b; b = temp;
 const int N=1e5 + 2;
 int mod(int a){
     if(a<0)
@@ -13,7 +13,7 @@ int gcd(int a,int b){
     return (b==0) ? (a) : (gcd(b,a%b));
 }
 void func1(){
-    unsigned long long n,m;cin>>n>>m;
+    unsigned long long int n,m;cin>>n>>m;
     if(n==m){
         cout<<0<<" "<<0;
     }
@@ -21,11 +21,11 @@ void func1(){
         if(n>m){
             swap(n,m);
         }
-        unsigned long long k = m-n;
+        unsigned long long int k = m-n;
         cout<<k<<" ";
-        int x = n/k;
-        unsigned long long diff1 = ((x+1)*k - n);
-        unsigned long long diff2 = (n-(k*x));
+        unsigned long long int x = n/k;
+        unsigned long long int diff1 = ((x+1)*k - n);
+        unsigned long long int diff2 = (n-(k*x));
         if(diff1<diff2){
             cout<<diff1;
         }
